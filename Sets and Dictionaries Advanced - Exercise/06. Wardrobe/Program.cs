144b20f0dd54d18dc094b204140b8425b0a333e0
EmanuilStefan <122681922+EmanuilStefan@users.noreply.github.com>
2024-01-30 19:47:47 +0200

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Exercise/06. Wardrobe/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(" -> ");
+            string color = input[0];
+            string[] clothes = input[1].Split(',');
+
+            if (!wardrobe.ContainsKey(color))
+            {
+                wardrobe[color] = new Dictionary<string, int>();
+            }
+
+            foreach (string cloth in clothes)
+            {
+                if (!wardrobe[color].ContainsKey(cloth))
+                {
+                    wardrobe[color][cloth] = 1;
+                }
+                else
+                {
+                    wardrobe[color][cloth]++;
+                }
+            }
+        }
+
+        string[] search = Console.ReadLine().Split(" ");
+        string colorToFind = search[0];
+        string clothesToFind = search[1];
+
+        foreach (var color in wardrobe.Keys)
+        {
+            Console.WriteLine($"{color} clothes:");
+
+            foreach (var item in wardrobe[color])
+            {
+                string itemName = item.Key;
+                int itemCount = item.Value;
+
+                if (itemName == clothesToFind && color == colorToFind)
+                {
+                    Console.WriteLine($"* {itemName} - {itemCount} (found!)");
+                }
+                else
+                {
+                    Console.WriteLine($"* {itemName} - {itemCount}");
+                }
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Exercise/07. The V-Logger/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, Vlogger> vloggers = new Dictionary<string, Vlogger>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "Statistics")
+        {
+            string[] tokens = input.Split();
+
+            if (tokens.Length == 4 && tokens[1] == "joined" && tokens[2] == "The" && tokens[3] == "V-Logger")
+            {
+                string vloggerName = tokens[0];
+                if (!vloggers.ContainsKey(vloggerName))
+                {
+                    vloggers.Add(vloggerName, new Vlogger());
+                }
+            }
+            else if (tokens.Length == 3 && tokens[1] == "followed")
+            {
+                string followerName = tokens[0];
+                string followedName = tokens[2];
+
+                if (vloggers.ContainsKey(followerName) && vloggers.ContainsKey(followedName) && followerName != followedName)
+                {
+                    vloggers[followerName].Follow(followedName);
+                    vloggers[followedName].AddFollower(followerName);
+                }
+            }
+        }
+
+        Console.WriteLine($"The V-Logger has a total of {vloggers.Count} vloggers in its logs.");
+
+        int counter = 1;
+        foreach (var vlogger in vloggers.OrderByDescending(v => v.Value.Followers.Count)
+                                       .ThenBy(v => v.Value.Following.Count))
+        {
+            Console.Write($"{counter}. {vlogger.Key} : {vlogger.Value.Followers.Count} followers, {vlogger.Value.Following.Count} following");
+
+            if (counter == 1)
+            {
+                Console.WriteLine();
+                foreach (var follower in vlogger.Value.Followers.OrderBy(f => f))
+                {
+                    Console.WriteLine($"*  {follower}");
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            counter++;
+        }
+    }
+}
+
+class Vlogger
+{
+    public HashSet<string> Followers { get; private set; }
+    public HashSet<string> Following { get; private set; }
+
+    public Vlogger()
+    {
+        Followers = new HashSet<string>();
+        Following = new HashSet<string>();
+    }
+
+    public void AddFollower(string follower)
+    {
+        Followers.Add(follower);
+    }
+
+    public void Follow(string vlogger)
+    {
+        Following.Add(vlogger);
+    }
+}
