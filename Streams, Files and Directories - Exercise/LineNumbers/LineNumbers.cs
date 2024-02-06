160f59a7eb069ebc64f19f40f2b24d9fc2d23ce6
EmanuilStefan <122681922+EmanuilStefan@users.noreply.github.com>
2024-02-06 17:56:52 +0200

    Add files via upload

---
+++ Streams, Files and Directories - Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,48 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string content = File.ReadAllText(inputFilePath);
+            string[] lines = content.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            Queue<int> lettersCount = new(); Queue<int> symbolsCount = new();
+            foreach (var line in lines)
+            {
+                int currentlettersCount = line.Where(x => char.IsLetter(x)).Count();
+                lettersCount.Enqueue(currentlettersCount); 
+                int currentsymbolsCount = line.Where(x => char.IsPunctuation(x)).Count();
+                symbolsCount.Enqueue(currentsymbolsCount);
+            }
+
+            StringBuilder stringBuilder = new();
+            int lineNumber = 1;
+
+            foreach (var line in lines)
+            {
+                stringBuilder.Append($"Line {lineNumber}: {line.Trim()}");                
+                stringBuilder.Append($" ({lettersCount.Dequeue()})({symbolsCount.Dequeue()})");
+                stringBuilder.Append(Environment.NewLine);
+                lineNumber++;
+            }
+
+            File.WriteAllText(outputFilePath, stringBuilder.ToString());
+
+            //Console.WriteLine(stringBuilder);
+        }
+    }
+}
---
+++ Streams, Files and Directories - Exercise/ZipAndExtract/ZipAndExtract .cs
@@ -0,0 +1,68 @@
+﻿namespace ZipAndExtract;
+
+
+using System;
+using System.IO;
+using System.IO.Compression;
+
+public class ZipAndExtract
+{
+    static void Main()
+    {
+        string inputFile = @"..\..\..\copyMe.png";
+        string zipArchiveFile = @"..\..\..\archive.zip";
+        string extractedFile = @"..\..\..\extracted.png";
+
+        ZipFileToArchive(inputFile, zipArchiveFile);
+
+        var fileNameOnly = Path.GetFileName(inputFile);
+        ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+    }
+
+    public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+    {
+        try
+        {
+            using (FileStream originalFileStream
+                = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream compressedFileStream = File.Create(zipArchiveFilePath))
+                {
+                    using (GZipStream zippedStream
+                        = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                    {
+                        originalFileStream.CopyTo(zippedStream);
+                    }
+                }
+            }
+            Console.WriteLine($"File Compressed successfully in {zipArchiveFilePath}.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error compressing file: {ex.Message}");
+        }
+    }
+    public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+    {
+        try
+        {
+            using (FileStream compressedFile = new FileStream(zipArchiveFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (GZipStream decompressedFile = new GZipStream(compressedFile, CompressionMode.Decompress))
+                {
+                    using (FileStream decompressedFileStream = File.Create(outputFilePath))
+                    {
+                        decompressedFile.CopyTo(decompressedFileStream);
+                    }
+                }
+            }
+
+            Console.WriteLine($"File extracted successfully to {outputFilePath}.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error extracting file: {ex.Message}");
+        }
+    }
+
+}
