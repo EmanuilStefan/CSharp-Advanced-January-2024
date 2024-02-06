72cd3aa8cfe297faa8c3123a693acec0e7472b63
EmanuilStefan <122681922+EmanuilStefan@users.noreply.github.com>
2024-02-06 17:55:26 +0200

    Add files via upload

---
+++ Streams, Files and Directories - Exercise/DirectoryTraversal/DirectoryTraversal.cs
@@ -0,0 +1,61 @@
+﻿namespace DirectoryTraversal;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+public class DirectoryTraversal
+{
+    static void Main()
+    {
+        string path = Console.ReadLine();
+        string reportFileName = @"\report.txt";
+
+        string reportContent = TraverseDirectory(path);
+        Console.WriteLine(reportContent);
+
+        WriteReportToDesktop(reportContent, reportFileName);
+    }
+
+    public static string TraverseDirectory(string inputFolderPath)
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(inputFolderPath);
+
+        // Get all files in the directory (first level only)
+        FileInfo[] files = directoryInfo.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+
+        // Group files by extension, order by count and then by name alphabetically
+        var groupedFiles = files
+            .GroupBy(file => file.Extension.ToLower())
+            .OrderByDescending(group => group.Count())
+            .ThenBy(group => group.Key);
+
+        StringBuilder reportContent = new StringBuilder();
+
+        foreach (var group in groupedFiles)
+        {
+            reportContent.AppendLine($"{group.Key}");
+
+            // Order files under each extension by size
+            var orderedFiles = group.OrderBy(file => file.Length);
+
+            foreach (var file in orderedFiles)
+            {
+                reportContent.AppendLine($"--{file.Name} - {file.Length / 1024.0:f3}kb");
+            }
+        }
+
+        return reportContent.ToString();
+    }
+
+    public static void WriteReportToDesktop(string textContent, string reportFileName)
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string reportFilePath = desktopPath + reportFileName;
+
+        File.WriteAllText(reportFilePath, textContent);
+
+        Console.WriteLine($"Report saved on the desktop: {reportFilePath}");
+    }
+}
---
+++ Streams, Files and Directories - Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,40 @@
+﻿namespace EvenLines;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+
+public class EvenLines
+{
+    static void Main()
+    {
+        string inputFilePath = @"..\..\..\text.txt";
+
+        Console.WriteLine(ProcessLines(inputFilePath));
+    }
+
+    public static string ProcessLines(string inputFilePath)
+    {
+        StringBuilder sb = new();
+        using (StreamReader sr = new StreamReader(inputFilePath))
+        {
+            while (!sr.EndOfStream)
+            {
+                sb.AppendLine(sr.ReadLine());
+            }
+        }
+        char[] symbols = new char[] { '-', ',', '.', '!', '?' };
+
+        foreach (char symbol in symbols)
+        {
+            sb.Replace(symbol, '@');
+        }
+
+        string[] processedText = sb.ToString().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string[] reversedProcessedText = processedText.Reverse().ToArray();
+
+        return string.Join(' ', reversedProcessedText);
+    }
+}
